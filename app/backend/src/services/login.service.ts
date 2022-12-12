8eba2bb847c74265d80db5bcc38db3ce60950201
Alef-Murick <alefcrow@gmail.com>
2022-12-12 20:11:14 -0300

    login service

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,8 +1,14 @@
-import { ILogin } from "../interfaces";
+import UsersModel from "../database/models/usersModel"
+import { ILogin } from "../interfaces"
+
 
 export default class LoginService {
   async login(login: ILogin) {
-    if (login) {
+    const email = await UsersModel.findOne({ where: {email: login.email}})
+    console.log('email~~~~~~~~~~~~~~', email);
+    
+    if (email) {
+      // GERAR TOKEN E ENVIAR TOKEN
       return { status: 200, message: 'random Token' }
     }
     return {status: 404, message: 'error'}
