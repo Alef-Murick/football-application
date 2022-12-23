159c800fdee7a14f0923720e1187f509c880e381
Alef-Murick <alefcrow@gmail.com>
2022-12-23 00:34:39 -0300

    validations login

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -6,18 +6,21 @@ import { validatePassword } from "../middlewares/validations";
 
 export default class LoginService {
   async login(login: iLogin): Promise<{ status: number, message: string }> {
-    const user = await User.findOne({ where: { email: login.email } })
-    const password = await user?.dataValues.password;
-    
-    if (user) {
-      const { message } = await validatePassword(password, login.password)
+    if (login.email || login.password) {  
+      const user = await User.findOne({ where: { email: login.email } })
+      const password = await user?.dataValues.password;
       
-      if (message === 'ok') {
-        const token = createToken(login)
-        return { status: 200, message: token }
+      if (user) {
+        const { message } = await validatePassword(password, login.password)
+        
+        if (message === 'ok') {
+          const token = createToken(login)
+          return { status: 200, message: token }
+        }
       }
-    }
-    return { status: 404, message: 'Incorrect email or password' }
+      return { status: 401, message: 'Incorrect email or password' }
+    } 
+    return { status: 400, message: 'All fields must be filled'}
   }
 }
 
