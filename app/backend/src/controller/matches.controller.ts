1b1284aba2f2d692fac07770e64fc47e79870498
Alef Murick <alefmurick@hotmail.com>
2023-01-03 16:38:42 -0300

    get matches

--- app/backend/src/controller/matches.controller.ts
+++ app/backend/src/controller/matches.controller.ts
@@ -2,20 +2,22 @@ import { Request, Response } from "express";
 import MatchesService from "../services/matches.service";
 
 export default class MatchesController {
-MatchesService = new MatchesService();
+  MatchesService = new MatchesService();
   
-  async getMatches(_req: Request, res: Response) {
-    const {status, message} = await this.MatchesService.findAllMatches();
-    return res.status(status).json(message);
-  }
+  async getMatches(req: Request, res: Response) {
+    const { inProgress } = req.query;
+    
+    if (inProgress === 'true') {
+      const { status, message } = await this.MatchesService.findLiveMatches();
+      return res.status(status).json(message);
+    }
 
-  async getLiveMatches(_req: Request, res: Response) {
-    const { status, message } = await this.MatchesService.findLiveMatches();
-    return res.status(status).json(message);
-  }
+    if (inProgress === 'false') {
+      const { status, message } = await this.MatchesService.findFinishedMatches();
+      return res.status(status).json(message);
+    }
 
-  async getFinishedMatches(_req: Request, res: Response) {
-    const { status, message } = await this.MatchesService.findLiveMatches();
+    const { status, message } = await this.MatchesService.findAllMatches();
     return res.status(status).json(message);
   }
 }
--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,19 +1,56 @@
 import { iMatch } from "../interfaces/iMatch";
 import Match from "../database/models/Match";
+import Team from "../database/models/Team";
 
 export default class MatchesService {
   async findAllMatches(): Promise<{ status: number, message: iMatch[] }> {
-    const matches = await Match.findAll();
+    const matches = await Match.findAll({
+      include: [
+        {
+          model: Team,
+          as: 'teamHome',
+        },
+        {
+          model: Team,
+          as: 'teamAway',
+        },
+
+      ],
+    });
     return { status: 200, message: matches}
   }
 
   async findLiveMatches(): Promise<{ status: number, message: string | iMatch[] }>  {
-    const matches = await Match.findAll({where: {inProgress: true }})
+    const matches = await Match.findAll({
+      where: { inProgress: true },
+      include: [
+        {
+          model: Team,
+          as: 'teamHome',
+        },
+        {
+          model: Team,
+          as: 'teamAway',
+        },
+
+      ],})
       return { status: 200, message: matches }
   }
 
 async findFinishedMatches(): Promise<{ status: number, message: string | iMatch[] }>  {
-  const matches = await Match.findAll({where: {inProgress: false }})
+  const matches = await Match.findAll({
+    where: { inProgress: false },
+    include: [
+      {
+        model: Team,
+        as: 'teamHome',
+      },
+      {
+        model: Team,
+        as: 'teamAway',
+      },
+
+    ],})
     return { status: 200, message: matches }
 }
 }
