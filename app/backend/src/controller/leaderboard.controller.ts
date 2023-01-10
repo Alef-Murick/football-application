193a64b6e0d27ce6ede644a137155011239b84bc
Alef Murick <alefmurick@hotmail.com>
2023-01-09 23:01:26 -0300

    leaderboard finalizado com apenas um controller hehe

--- app/backend/src/controller/leaderboard.controller.ts
+++ app/backend/src/controller/leaderboard.controller.ts
@@ -4,8 +4,13 @@ import LeaderboardService from "../services/leaderboard.service";
 export default class LeaderboardController {
   leaderboardService = new LeaderboardService();
 
-  async getLeaderboard(_req: Request, res: Response) {
-    const leaderboard = await this.leaderboardService.getLeaderboards();
+  async getLeaderboard(req: Request, res: Response) {
+    const path = req.path
+    console.log('path', path);
+    
+    const leaderboard = await this.leaderboardService.getLeaderboards(path);
+    // console.log('LEADERBOARD IN CONTROLLER~~~~~~~~~~~~~~', leaderboard);
+    
     return res.status(200).json(leaderboard);
   };
 }
--- app/backend/src/services/leaderboard.service.ts
+++ app/backend/src/services/leaderboard.service.ts
@@ -1,11 +1,13 @@
 import MatchesService from "./matches.service";
 
 export default class LeaderboardService extends MatchesService {
-  async getLeaderboards() {
+  async getLeaderboards(path: string) {
     const matches = await this.findFinishedMatches()
+    // console.log('MATCHES >>>>>', matches);
     const teams = await this.teamService.getAllTeams();
+    // console.log('TEAMS >>>>>', teams);    
     const leaderboard = teams.map((team) => {
-      
+      // console.log('LEADERBOARD >>>>>', leaderboard);      
       const table = {
         name: team.teamName,
         totalVictories: 0,
@@ -18,14 +20,12 @@ export default class LeaderboardService extends MatchesService {
         goalsBalance: 0,
         efficiency: 0,
       }
-
+      // console.log('MATCHES MESSAGE >>>>>', matches.message);      
       if (typeof matches.message !== "string") {
-
-        matches.message.forEach(async (match) => {
-          const homeTeam = await this.teamService.getTeamById(match.homeTeam);
-          const awayTeam = await this.teamService.getTeamById(match.awayTeam);
-          if (typeof homeTeam.message !== "string") {
-            if (team.teamName === homeTeam.message.teamName) {
+        if (path === '/home') {          
+          matches.message.forEach(async (match) => {
+            if (team.id === match.homeTeam) {
+              
               if (match.homeTeamGoals > match.awayTeamGoals) {
                 table.totalVictories += 1;
                 table.totalPoints += 3;
@@ -43,11 +43,79 @@ export default class LeaderboardService extends MatchesService {
               table.goalsBalance = table.goalsFavor - table.goalsOwn;
               table.efficiency = Number(((table.totalPoints / (table.totalGames * 3)) * 100).toFixed(2))
             }
-          }
-        });
+          });
+        }
+
+        if (path === '/away') { 
+          matches.message.forEach(async (match) => {
+            if (team.id === match.awayTeam) {
+              
+              if (match.awayTeamGoals > match.homeTeamGoals) {
+                table.totalVictories += 1;
+                table.totalPoints += 3;
+              }
+              if (match.awayTeamGoals < match.homeTeamGoals) {
+                table.totalLosses += 1;
+              }
+              if (match.awayTeamGoals === match.homeTeamGoals) {
+                table.totalDraws += 1;
+                table.totalPoints += 1;
+              }
+              table.totalGames += 1;
+              table.goalsFavor += match.awayTeamGoals;
+              table.goalsOwn += match.homeTeamGoals;
+              table.goalsBalance = table.goalsFavor - table.goalsOwn;
+              table.efficiency = Number(((table.totalPoints / (table.totalGames * 3)) * 100).toFixed(2))
+            }
+          });
+        }
+
+        if (path === '/') {
+          matches.message.forEach(async (match) => {
+            if (team.id === match.homeTeam) {              
+              if (match.homeTeamGoals > match.awayTeamGoals) {
+                table.totalVictories += 1;
+                table.totalPoints += 3;
+              }
+              if (match.homeTeamGoals < match.awayTeamGoals) {
+                table.totalLosses += 1;
+              }
+              if (match.homeTeamGoals === match.awayTeamGoals) {
+                table.totalDraws += 1;
+                table.totalPoints += 1;
+              }
+              table.totalGames += 1;
+              table.goalsFavor += match.homeTeamGoals;
+              table.goalsOwn += match.awayTeamGoals;
+              table.goalsBalance = table.goalsFavor - table.goalsOwn;
+              table.efficiency = Number(((table.totalPoints / (table.totalGames * 3)) * 100).toFixed(2))
+            }
+            if (team.id === match.awayTeam) {
+              
+              if (match.awayTeamGoals > match.homeTeamGoals) {
+                table.totalVictories += 1;
+                table.totalPoints += 3;
+              }
+              if (match.awayTeamGoals < match.homeTeamGoals) {
+                table.totalLosses += 1;
+              }
+              if (match.awayTeamGoals === match.homeTeamGoals) {
+                table.totalDraws += 1;
+                table.totalPoints += 1;
+              }
+              table.totalGames += 1;
+              table.goalsFavor += match.awayTeamGoals;
+              table.goalsOwn += match.homeTeamGoals;
+              table.goalsBalance = table.goalsFavor - table.goalsOwn;
+              table.efficiency = Number(((table.totalPoints / (table.totalGames * 3)) * 100).toFixed(2))
+            }
+          });
+        }
       }
+        // console.log('TABLE FILLED?3333333333', table);
       return table;
     });
+    // console.log('leaderboard return', leaderboard);
     return leaderboard.sort((a, b) =>
       b.totalPoints - a.totalPoints
       || b.totalVictories - a.totalVictories
